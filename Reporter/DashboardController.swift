e823d1022389c367923fd5fad458d2f9126e036b
Parth Tamane <parthv21@gmail.com>
2019-08-28 23:02:26 -0400

    Initial Setup

---
+++ Reporter/DashboardController.swift
@@ -0,0 +1,17 @@
+ //
+//  Dashboard.swift
+//  Reporter
+//
+//  Created by Parth Tamane on 28/08/19.
+//  Copyright © 2019 Parth Tamane. All rights reserved.
+//
+
+import Foundation
+import UIKit
+ 
+ class DashboardController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+ }
